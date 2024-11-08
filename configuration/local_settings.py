40a1206ddcd2b6fae5990224ecacbc742f9639a1
Eugene de Beste <eugene.debeste@nexgencloud.com>
2024-11-08 10:50:05 +0200

    provisional branching plugin support added

---
+++ configuration/local_settings.py
@@ -0,0 +1,16 @@
+### Branching plugin settings
+
+from netbox_branching.utilities import DynamicSchemaDict
+from .configuration import DATABASE
+
+# Wrap DATABASES with DynamicSchemaDict for dynamic schema support
+DATABASES = DynamicSchemaDict({
+    'default': DATABASE,
+})
+
+# Employ our custom database router
+DATABASE_ROUTERS = [
+    'netbox_branching.database.BranchAwareRouter',
+]
+
+###
--- configuration/plugins.py
+++ configuration/plugins.py
@@ -1,1 +1,6 @@
-PLUGINS = ['netbox_bgp','netbox_topology_views']
+PLUGINS = [
+    'netbox_bgp',
+    'netbox_topology_views',
+    # Must be last plugin - https://github.com/netboxlabs/netbox-branching?tab=readme-ov-file#installation
+    'netbox_branching'
+]
