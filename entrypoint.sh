c30a5bc911a2ba9a008640ac62626f20386a3db7
Eugene de Beste <eugene.debeste@nexgencloud.com>
2024-11-11 14:43:39 +0200

    provisional branching plugin support added (#3)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/opt/netbox/venv/bin/python manage.py migrate netbox_topology_views
+/opt/netbox/venv/bin/python manage.py migrate
 /opt/netbox/venv/bin/python manage.py collectstatic --no-input
 /opt/netbox/docker-entrypoint.sh /opt/netbox/launch-netbox.sh
